3b1cd793f78644a6325c0a313f8313b90d38f144
paramjeetug21 <164073819+paramjeetug21@users.noreply.github.com>
2025-01-01 08:35:08 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { TodoList } from "./components/todoList";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="todo-app">
+        <TodoList></TodoList>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { TodoForm } from "./todoForm";
+import { Todo } from "./todo";
+
+export const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text) return;
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text) return;
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = todos.filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>Hey, what's the plan for today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+};
